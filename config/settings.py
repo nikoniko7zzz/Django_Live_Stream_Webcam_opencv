62f007029ae9c5ca5d8577fdeabdcf8c7c3c8023
matsuo <matsuokuniko7@gmail.com>
2022-09-13 16:21:36 +0900

    Re-create SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+k8hh9z+%v$umfe&*o9-%_m)30dgak$7sd^mq8c(y(0+p97ro$'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
